b1efefa36cb7491ea084ab820416f734a158ec7f
Alexander Le <alexanderle0817@gmail.com>
2019-07-31 21:13:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// Create an Express server that can respond to requests for teams information
+//Get /teams- returns full list of teams
+// Get/teams/X - returns team with their ID, where X is the ID number
+// Get/teams/ABBR - returns team associated with abbreviation
