54956037ee2bc8a6685f8b5633797cc7770291b8
Alexander Le <alexanderle0817@gmail.com>
2019-07-31 21:37:27 -0400

    implement get teams

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 //Get /teams- returns full list of teams
 // Get/teams/X - returns team with their ID, where X is the ID number
 // Get/teams/ABBR - returns team associated with abbreviation
+
+let express = require('express')
+let teams = require('./teams.json');
+
+let app = express();
+
+app.get("/teams", (request,response) => {
+    response.send(teams)
+})
